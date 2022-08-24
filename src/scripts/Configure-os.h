5e3003cbe5c8580731740b15376b4f6ce2f662fc
Martin D Kealey <martin@kurahaupo.gen.nz>
2022-08-24 17:27:36 +1000

    Shell linting

--- src/scripts/Configure-os.h
+++ src/scripts/Configure-os.h
@@ -6,46 +6,47 @@ scripts=../scripts
 
 # Get the OS type, and check that there is a make file for it.
 
-os=`$scripts/os-type -generic` || exit 1
+os=$( $scripts/os-type -generic ) || exit 1
 
-if	test ! -r ../OS/Makefile-$os
-then    echo ""
+if	test ! -r "../OS/Makefile-$os"
+then	echo
 	echo "*** Sorry - operating system $os is not supported"
-        echo "*** See OS/Makefile-* for supported systems" 1>&2
-        echo ""
-	exit 1;
-fi
+	echo "*** See OS/Makefile-* for supported systems" 1>&2
+	echo
+	exit 1
+fi >&2
 
 # Ensure there is an OS-specific header file, and link it to os.h. There should
 # always be one if there is a make file for the OS, so its absence is somewhat
 # disastrous.
 
-if	test ! -r ../OS/os.h-$os
-then    echo ""
+if	test ! -r "../OS/os.h-$os"
+then	echo
 	echo "*** Build error: OS/os.h-$os file is missing"
-        echo ""
-	exit 1;
-fi
+	echo
+	exit 1
+fi >&2
 rm -f os.h
 
 # In order to accommodate for the fudge below, copy the file instead of
 # symlinking it. Otherwise we pollute the clean copy with the fudge.
-cp -p ../OS/os.h-$os os.h || exit 1
+cp -p "../OS/os.h-$os" os.h || exit 1
 
 # Special-purpose fudge for older versions of Linux (pre 2.1.15) that
 # use the structure name "options" instead of "ip_options".
 
-if [ "$os" != "Linux" -a "$os" != "Linux-libc5" ] ; then exit 0; fi
+case "$os" in
+Linux | Linux-libc5)
+	if	! grep ip_options /usr/include/linux/ip.h >/dev/null 2>&1
+	then	cat >>os.h <<-End
 
-grep ip_options /usr/include/linux/ip.h >/dev/null
-if [ $? = 0 ] ; then exit 0; fi
+		/* Fudge added because this Linux doesn't appear to have a definition
+		for ip_options in /usr/include/linux/ip.h. */
 
-cat >>os.h <<End
-
-/* Fudge added because this Linux doesn't appear to have a definition
-for ip_options in /usr/include/linux/ip.h. */
-
-#define ip_options options
-End
+		#define ip_options options
+		End
+	fi
+	;;
+esac
 
 # End of Configure-os.h
